412f1b5a1d5dbf5c032832f0d27105a1f4cea2b4
Vatsal Kadiya <vatsalkadiya@gmail.com>
2025-12-16 22:15:34 +0530

    Todo API with MySQL CRUD working

--- db.py
+++ db.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 db_password = os.getenv("DATABASE_PASSWORD")
 db_name = os.getenv("DATABASE_NAME")
+db_user = os.getenv("DATABASE_USER")
+db_port = os.getenv("DATABASE_PORT")
+db_host = os.getenv("DATABASE_HOST")
 
 if not db_password or not db_name:
     raise ValueError("Missing database environment variables")
@@ -14,9 +17,9 @@ if not db_password or not db_name:
 def get_connection():
     try:
         db = mysql.connector.connect(
-            host="127.0.0.1",
-            port=3306,
-            user="root",
+            host=db_host,
+            port=db_port,
+            user=db_user,
             passwd=db_password,
             database=db_name,
             auth_plugin="mysql_native_password",
